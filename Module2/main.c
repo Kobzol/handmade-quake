48b67216d230f479d66febc0ee125b4ec17462d1
Jakub Beránek <berykubik@gmail.com>
2016-01-21 21:05:58 +0100

    ENH: added basic message queue

--- Module2/main.c
+++ Module2/main.c
@@ -1,13 +1,26 @@
 #include <windows.h>
 
+BOOL IsRunning = TRUE;
+
+
+
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	LRESULT result = 0;
+
 	switch (uMsg)
 	{
-
+		case WM_KEYUP:
+			IsRunning = FALSE;
+			break;
+		case WM_DESTROY:
+			IsRunning = FALSE;
+			break;
+		default:
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return result;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -43,6 +56,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	HDC deviceContext = GetDC(mainWindow);
 	PatBlt(deviceContext, 0, 0, width, height, BLACKNESS);
 	ReleaseDC(mainWindow, deviceContext);
+
+	MSG message;
+
+	while (IsRunning)
+	{
+		if (PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+	}
 
 	return 0;
 }
