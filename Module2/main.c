bdf62bb5003a2a54d8053cd1ddf9b3da73d39513
Jakub Beránek <berykubik@gmail.com>
2016-01-28 17:05:15 +0100

    ENH: added timing functions

--- Module2/main.c
+++ Module2/main.c
@@ -1,7 +1,50 @@
 #include <windows.h>
 #include <stdio.h>
 
-BOOL IsRunning = TRUE;
+static BOOL IsRunning = TRUE;				// controls whether the game loop is running
+
+void Sys_Shutdown(void)
+{
+	IsRunning = FALSE;
+
+	PostQuitMessage(0);
+}
+
+/////////////////////////////////
+//		TIMER CODE
+/////////////////////////////////
+
+static double GTimePassed = 0.0;			// total passed time from the start of the game in seconds
+static double SecondsPerTick = 0.0;			// seconds passed per tick
+static __int64 LastTick = 0;				// time of the last tick
+
+// initializes the game time
+float Sys_InitFloatTime(void)
+{
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	SecondsPerTick = 1.0 / frequency.QuadPart;
+
+	LARGE_INTEGER time;
+	QueryPerformanceCounter(&time);
+	LastTick = time.QuadPart;
+
+	return 0.0f;
+}
+
+float Sys_FloatTime(void)
+{
+	LARGE_INTEGER time;
+	QueryPerformanceCounter(&time);
+
+	__int64 interval = time.QuadPart - LastTick;
+	LastTick = time.QuadPart;
+	
+	double delta = interval * SecondsPerTick;
+	GTimePassed += delta;
+
+	return (float) GTimePassed;
+}
 
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -9,11 +52,12 @@ LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	switch (uMsg)
 	{
-		case WM_KEYUP:
-			IsRunning = FALSE;
+		case WM_ACTIVATE:
+			break;
+		case WM_CREATE:
 			break;
 		case WM_DESTROY:
-			IsRunning = FALSE;
+			Sys_Shutdown();
 			break;
 		default:
 			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -50,40 +94,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	width = rectangle.right - rectangle.left;
 	height = rectangle.bottom - rectangle.top;
 
-	mainWindow = CreateWindowEx(0, "Module 2", "Lesson 2.3", windowStyle, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, hInstance, NULL);
+	mainWindow = CreateWindowEx(0, "Module 2", "Lesson 2.4", windowStyle, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, hInstance, NULL);
 
 	HDC deviceContext = GetDC(mainWindow);
 	PatBlt(deviceContext, 0, 0, width, height, BLACKNESS);
 	ReleaseDC(mainWindow, deviceContext);
 
-	LARGE_INTEGER frequency;
-	QueryPerformanceFrequency(&frequency);
-
-	double secondsPerTick = 1.0 / frequency.QuadPart;
-
-	LARGE_INTEGER tick, tock;
-	QueryPerformanceCounter(&tick);
+	float time = Sys_InitFloatTime();
 
 	MSG message;
 
 	while (IsRunning)
 	{
-		if (PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
+		while (PeekMessage(&message, NULL, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
 
-		QueryPerformanceCounter(&tock);
-
-		__int64 interval = tock.QuadPart - tick.QuadPart;
-		double secondsGoneBy = interval * secondsPerTick;
+		float new_time = Sys_FloatTime();
 
 		char buf[64];
-		sprintf_s(buf, 64, "Total time: %3.7f \n", secondsGoneBy);
+		sprintf_s(buf, 64, "Total time: %3.7f \n", new_time);
 		OutputDebugString(buf);
 
-		QueryPerformanceCounter(&tick);
+		Sleep(1);
 	}
 
 	return 0;
