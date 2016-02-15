8f3891a76bdd465fcb3eb3b00c9ee65256f77c00
Jakub Beránek <berykubik@gmail.com>
2016-02-15 20:41:31 +0100

    ENH: added 8-bit indexed rendering

--- Module3/sys_win.c
+++ Module3/sys_win.c
@@ -1,13 +1,83 @@
 #include <Windows.h>
+#include <time.h>
 
-int Running = 1;
+unsigned int Running = 1;
 
-int BufferWidth = 640;
-int BufferHeight = 480;
+unsigned int BufferWidth = 640;
+unsigned int BufferHeight = 480;
+unsigned int BytesPerPixel = 1;
+typedef unsigned char buffer_t;
 
-void* BackBuffer = NULL;
+buffer_t* BackBuffer = NULL;
+BOOL Fullscreen = FALSE;
 
-BITMAPINFO BitMapInfo = { 0 };
+typedef struct
+{
+	BITMAPINFOHEADER	bmiHeader;
+	RGBQUAD				acolors[256];
+} dibinfo_t;
+
+dibinfo_t BitMapInfo = { 0 };
+
+void DrawRect(buffer_t* buffer, unsigned int x, unsigned int y, unsigned int width, unsigned int height,
+	unsigned char red, unsigned char green, unsigned char blue)
+{
+	if (x >= BufferWidth) return;
+	if (y >= BufferHeight) return;
+
+	if (x + width >= BufferWidth)
+	{
+		width = BufferWidth - x - 1;
+	}
+	if (y + height >= BufferHeight)
+	{
+		height = BufferHeight - y - 1;
+	}
+
+	unsigned int color = (red << 16) | (green << 8) | blue;
+	buffer_t* mem = buffer;
+
+	mem += (BufferWidth * y + x) * BytesPerPixel;  // move to first pixel
+
+	for (unsigned int i = 0; i < height; i++)
+	{
+		for (unsigned int j = 0; j < width; j++)
+		{
+			*mem = color;
+			mem++;
+		}
+		mem += BufferWidth - width;  // skip row and move to first column
+	}
+}
+void DrawRect8(buffer_t* buffer, unsigned int x, unsigned int y, unsigned int width, unsigned int height,
+	buffer_t colorIndex)
+{
+	if (x >= BufferWidth) return;
+	if (y >= BufferHeight) return;
+
+	if (x + width >= BufferWidth)
+	{
+		width = BufferWidth - x - 1;
+	}
+	if (y + height >= BufferHeight)
+	{
+		height = BufferHeight - y - 1;
+	}
+
+	buffer_t* mem = buffer;
+
+	mem += (BufferWidth * y + x) * BytesPerPixel;  // move to first pixel
+
+	for (unsigned int i = 0; i < height; i++)
+	{
+		for (unsigned int j = 0; j < width; j++)
+		{
+			*mem = colorIndex;
+			mem++;
+		}
+		mem += BufferWidth - width;  // skip row and move to first column
+	}
+}
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -28,6 +98,8 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	srand((unsigned int) time(NULL));
+
 	// window class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
@@ -39,8 +111,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	DWORD windowStyleEx = 0;
 	DWORD windowStyle = WS_OVERLAPPEDWINDOW;
-
-	BOOL Fullscreen = TRUE;
 
 	if (Fullscreen)
 	{
@@ -94,12 +164,26 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	// bitmap info
 	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
 	BitMapInfo.bmiHeader.biWidth = BufferWidth;
-	BitMapInfo.bmiHeader.biHeight = BufferHeight;
+	BitMapInfo.bmiHeader.biHeight = - (int) BufferHeight;
 	BitMapInfo.bmiHeader.biPlanes = 1;
-	BitMapInfo.bmiHeader.biBitCount = 32;
+	BitMapInfo.bmiHeader.biBitCount = 8 * BytesPerPixel;
 	BitMapInfo.bmiHeader.biCompression = BI_RGB;
 
-	BackBuffer = malloc(BufferWidth * BufferHeight * 4);
+	BackBuffer = malloc(BufferWidth * BufferHeight * BytesPerPixel);
+
+	if (BytesPerPixel == 1)
+	{
+		BitMapInfo.acolors[0].rgbRed = 0;
+		BitMapInfo.acolors[0].rgbGreen = 0;
+		BitMapInfo.acolors[0].rgbBlue = 0;
+
+		for (int i = 1; i < 256; i++)
+		{
+			BitMapInfo.acolors[i].rgbRed = rand() % 256;
+			BitMapInfo.acolors[i].rgbGreen = rand() % 256;
+			BitMapInfo.acolors[i].rgbBlue = rand() % 256;
+		}
+	}
 
 	// message loop
 	MSG message;
@@ -113,20 +197,22 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 			DispatchMessage(&message);
 		}
 
-		unsigned int* bufferMemory = (unsigned int*)BackBuffer;
-		for (int y = 0; y < BufferHeight; y++)
+		buffer_t* bufferMemory = BackBuffer;
+		for (unsigned int y = 0; y < BufferHeight; y++)
 		{
-			for (int x = 0; x < BufferWidth; x++)
+			for (unsigned int x = 0; x < BufferWidth; x++)
 			{
-				unsigned int red = rand() % 256;
-				unsigned int green = rand() % 256;
-				unsigned int blue = rand() % 256;
-
-				*bufferMemory++ = (red << 16) | (green << 8) | blue;
+				*bufferMemory++ = rand() % 256;
 			}
 		}
 
-		StretchDIBits(hdc, 0, 0, BufferWidth, BufferHeight, 0, 0, BufferWidth, BufferHeight, BackBuffer, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+		if (BytesPerPixel == 4)
+		{
+			DrawRect(BackBuffer, 10, 10, 300, 200, 255, 0, 0);
+		}
+		else DrawRect8(BackBuffer, 10, 10, 300, 150, 1);
+
+		StretchDIBits(hdc, 0, 0, BufferWidth, BufferHeight, 0, 0, BufferWidth, BufferHeight, BackBuffer, (BITMAPINFO*) &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
 	}
 
 	DeleteDC(hdc);
