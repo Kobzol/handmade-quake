906fcca32d104591a4a6a81c865dae92c4e17610
Jakub Beránek <berykubik@gmail.com>
2016-02-25 19:21:38 +0100

    ENH: added video mode quering and fullscreen switching

--- handmade-quake/vid.h
+++ handmade-quake/vid.h
@@ -4,7 +4,23 @@
 #include "winquake.h"
 #include "sys.h"
 
+typedef enum
+{
+	MS_WINDOWED,
+	MS_FULLSCREEN
+} modestate_t;
+
+typedef struct
+{
+	modestate_t type;
+	uint32 width;
+	uint32 height;
+	uint32 frequency;
+} vmode_t;
+
 void VID_Init(void);
-void VID_SetMode(uint32 width, uint32 height);
+void VID_InitWindowedMode(void);
+void VID_InitFullscreenMode(void);
+void VID_SetMode(uint32 mode_index);
 void VID_Update(void);
 void VID_Shutdown(void);
--- handmade-quake/vid_win.c
+++ handmade-quake/vid_win.c
@@ -7,9 +7,13 @@ static uint32 WindowWidth = 800;
 static uint32 BufferWidth = 320;
 static uint32 BufferHeight = 240;
 
-void* BackBuffer = NULL;
+static void* BackBuffer = NULL;
 
-BITMAPINFO BitMapInfo = { 0 };
+static BITMAPINFO BitMapInfo = { 0 };
+static vmode_t Modelist[50];
+static uint32 ModeCount = 0;
+static uint32 ModeActive = 0;
+static uint32_t FirstFullscreenMode = 0;
 
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -28,15 +32,15 @@ LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		if (wParam == 'A')
 		{
-			VID_SetMode(640, 480);
+			VID_SetMode(0);
 		}
 		else if (wParam == 'S')
 		{
-			VID_SetMode(800, 600);
+			VID_SetMode(FirstFullscreenMode);
 		}
 		else if (wParam == 'D')
 		{
-			VID_SetMode(1024, 768);
+			VID_SetMode(2);
 		}
 	}
 		break;
@@ -47,14 +51,40 @@ LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return result;
 }
 
-void VID_SetMode(uint32 width, uint32 height)
+void VID_SetMode(uint32_t mode_index)
 {
-	RECT rectangle;
-	GetWindowRect(MainWindow, &rectangle);
-	MoveWindow(MainWindow, rectangle.left, rectangle.top, width, height, TRUE);
+	uint32 width = Modelist[mode_index].width;
+	uint32 height = Modelist[mode_index].height;
+
+	vmode_t mode = Modelist[mode_index];
+
+	if (mode.type == MS_FULLSCREEN)
+	{
+		DEVMODE screenSettings = { 0 };
+		screenSettings.dmSize = sizeof(screenSettings);
+		screenSettings.dmPelsWidth = width;
+		screenSettings.dmPelsHeight = height;
+		screenSettings.dmBitsPerPel = 32;
+		screenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
+	}
+	else
+	{
+		if (Modelist[ModeActive].type == MS_FULLSCREEN)
+		{
+			ChangeDisplaySettings(NULL, 0);
+		}
+
+		RECT rectangle;
+		GetWindowRect(MainWindow, &rectangle);
+		MoveWindow(MainWindow, rectangle.left, rectangle.top, width, height, TRUE);
+	}
 
 	WindowWidth = width;
 	WindowHeight = height;
+
+	ModeActive = mode_index;
 }
 
 void VID_Init(void)
@@ -64,7 +94,7 @@ void VID_Init(void)
 	wc.lpfnWndProc = MainWndProc;
 	wc.hInstance = GlobalInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.lpszClassName = "Module 2";
+	wc.lpszClassName = "Module 3";
 
 	if (!RegisterClassEx(&wc))
 	{
@@ -73,28 +103,6 @@ void VID_Init(void)
 
 	DWORD windowStyleEx = 0;
 	DWORD windowStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
-
-	/*BOOL fullscreen;
-
-	if (fullscreen)
-	{
-	DEVMODE screenSettings = { 0 };
-	screenSettings.dmSize = sizeof(screenSettings);
-	screenSettings.dmPelsWidth = WindowWidth;
-	screenSettings.dmPelsHeight = WindowHeight;
-	screenSettings.dmBitsPerPel = 32;
-	screenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
-
-	if (ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN) == DISP_CHANGE_SUCCESSFUL)
-	{
-	windowStyleEx = WS_EX_APPWINDOW;
-	windowStyle = WS_POPUP;
-	}
-	else
-	{
-	Fullscreen = FALSE;
-	}
-	}*/
 
 	RECT windowInnerSize = { 0 };
 	windowInnerSize.right = WindowWidth;
@@ -105,7 +113,7 @@ void VID_Init(void)
 	uint32 width = windowInnerSize.right - windowInnerSize.left;
 	uint32 height = windowInnerSize.bottom - windowInnerSize.top;
 
-	MainWindow = CreateWindowEx(windowStyleEx, "Module 2", "Lesson 2.6", windowStyle, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, GlobalInstance, NULL);
+	MainWindow = CreateWindowEx(windowStyleEx, "Module 3", "Lesson 3.6", windowStyle, CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL, GlobalInstance, NULL);
 
 	HDC deviceContext = GetDC(MainWindow);
 	PatBlt(deviceContext, 0, 0, width, height, BLACKNESS);
@@ -114,12 +122,83 @@ void VID_Init(void)
 	// bitmap info
 	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
 	BitMapInfo.bmiHeader.biWidth = BufferWidth;
-	BitMapInfo.bmiHeader.biHeight = -(int)BufferHeight;
+	BitMapInfo.bmiHeader.biHeight = - (int32) BufferHeight;
 	BitMapInfo.bmiHeader.biPlanes = 1;
 	BitMapInfo.bmiHeader.biBitCount = 8;
 	BitMapInfo.bmiHeader.biCompression = BI_RGB;
 
 	BackBuffer = malloc(BufferWidth * BufferHeight);
+
+	VID_InitWindowedMode();
+	VID_InitFullscreenMode();
+
+	VID_SetMode(0);
+}
+void VID_InitWindowedMode(void)
+{
+	Modelist[ModeCount].type = MS_WINDOWED;
+	Modelist[ModeCount].width = 320;
+	Modelist[ModeCount].height = 240;
+	Modelist[ModeCount].frequency = 60;
+	ModeCount++;
+
+	Modelist[ModeCount].type = MS_WINDOWED;
+	Modelist[ModeCount].width = 640;
+	Modelist[ModeCount].height = 480;
+	Modelist[ModeCount].frequency = 60;
+	ModeCount++;
+
+	Modelist[ModeCount].type = MS_WINDOWED;
+	Modelist[ModeCount].width = 800;
+	Modelist[ModeCount].height = 600;
+	Modelist[ModeCount].frequency = 60;
+	ModeCount++;
+
+	Modelist[ModeCount].type = MS_WINDOWED;
+	Modelist[ModeCount].width = 1024;
+	Modelist[ModeCount].height = 768;
+	Modelist[ModeCount].frequency = 60;
+	ModeCount++;
+}
+
+void VID_InitFullscreenMode(void)
+{
+	FirstFullscreenMode = ModeCount;
+
+	DEVMODE devmode;
+	uint32 modeNum = 0;
+	BOOL success;
+	uint32 last_width = 0, last_height = 0;
+
+	do
+	{
+		success = EnumDisplaySettings(NULL, modeNum++, &devmode);
+
+		if (!success)
+		{
+			break;
+		}
+
+		if (devmode.dmDisplayFrequency >= 59)
+		{
+			if (devmode.dmPelsWidth != last_width || devmode.dmPelsHeight != last_height)
+			{
+				Modelist[ModeCount].type = MS_FULLSCREEN;
+				Modelist[ModeCount].width = devmode.dmPelsWidth;
+				Modelist[ModeCount].height = devmode.dmPelsHeight;
+				Modelist[ModeCount].frequency = devmode.dmDisplayFrequency;
+				ModeCount++;
+
+				last_width = devmode.dmPelsWidth;
+				last_height = devmode.dmPelsHeight;
+			}
+			else if (devmode.dmDisplayFrequency > Modelist[ModeCount - 1].frequency)
+			{
+				Modelist[ModeCount - 1].frequency = devmode.dmDisplayFrequency;
+			}
+		}
+
+	} while (success);
 }
 
 void VID_Update(void)
